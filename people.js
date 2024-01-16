ed82360b52039a9a33c6c4359052d931e0edbe77
DiptiranjanMahakud <diptiranjanmohakud304@gmail.com>
2024-01-16 12:27:52 +0530

    Added

---
+++ people.js
@@ -0,0 +1,6 @@
+const people=['yoshi','ryu','chun-li','marie'];
+
+const ages=[20,30];
+//console.log(people);
+
+module.exports={people,ages};
---
+++ server.js
@@ -0,0 +1,44 @@
+const http=require('http');
+const fs=require('fs');
+const _ = require('lodash');
+
+const server=http.createServer((req,res)=>{
+    //loadash
+    const num=_.random(0,20);
+    console.log(num);
+
+     res.setHeader('Content-type','text/html');
+
+
+    let path='./views/';
+    switch(req.url){
+        case'/':path+='index.html';
+        res.statusCode=200;
+        break;
+        case '/about':path+='about.html';
+        res.statusCode=200;
+        break;
+        case '/about-blah':
+            res.statusCode=301;
+            res.setHeader('Location','/about');
+            res.end();
+        default:path+='404.html';
+        break;
+    }
+
+
+    //send a html file 
+    fs.readFile(path,(err,data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            //res.write(data);
+            res.end(data);
+        }
+    })
+});
+
+server.listen(3000,'localhost',()=>{
+    console.log('listening for request on port 3000');
+});
